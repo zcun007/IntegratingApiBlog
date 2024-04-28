cec59a8ddddba4592b7a0e1ed4adb678183723a8
zcun007 <160480881+zcun007@users.noreply.github.com>
2024-04-28 22:54:10 +0000

    Urls, views and index.html files updated

--- django_project/views.py
+++ django_project/views.py
@@ -1,17 +1,37 @@
-from django.shortcuts import render
 import requests
+from django.shortcuts import render
+
+
 
 def index(request):
-  r1 = requests.get('https://api.github.com/events')
-  data = r1.json()
-  events = data[0]['repo']
-  
-  r2 = requests.get('https://www.boredapi.com/api/activity')
-  data = r2.json()
-  activity  = data['activity']
+    r1 = requests.get('	https://random-data-api.com/api/v2/addresses/full_address')
+    data = r1.json()
+    full_address = data['full_address']
+  if  r1.status_code == requests.codes.ok:
+    print(r1.text)
+  else:
+    print("Error:", r1.status_code, r1.text)    
+ 
+    r2 = requests.get('https://random-data-api.com/api/v2/blood_types/type')
+    blood_type = r2.json()
+    type = blood_type['type']
+  if  r2.status_code == requests.codes.ok:
+    print(r2.text)
+  else:
+    print("Error:", r2.status_code, r2.text)
 
-  r3 = requests.get('https://dog.ceo/api/breeds/image/random')
-  res3 = r3.json()
-  dog = res3['message']
+    r3 = requests.get('https://random-data-api.com/api/v2/credit_cards/credit_card_number')
+    res3 = r3.json()
+    card = res3['credit_card_number']
+  if r3.status_code == requests.codes.ok:
+    print(r3.text)
+  else:
+    print("Error:", r3.status_code, r3.text)
+    
+  return render(request, 'templates/index.html', {
+      'full_address': full_address,
+      'type': type,
+      'card': card
+  })
+
   
-  return render(request, 'templates/index.html', {'events': events, 'activity': activity, 'dog': dog})
